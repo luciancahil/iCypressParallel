611e26d4d593a2dc9a81f5aab95dcb4e2e7b3de1
luciancahil <royhe62@yahoo.ca>
2024-01-07 01:31:37 -0800

    Added patientlines method to ADP

--- run/AnnDataProcessor.py
+++ run/AnnDataProcessor.py
@@ -18,8 +18,28 @@ class AnnDataProcessor:
 
         self.geneArray = [x[0] for x in self.geneArray]
 
-        print(self.geneArray)
+    """
+    Returns a list of patient lines structured as if they were just read from a csv
+    """
+    def patientLines(self):
+        classifaction_map = {} # maps the string describing the ailment to the number its assigned to.
+        lines = []
+        for i in range(self.num_patients):
+            classification = self.y_data_frame.iloc[i]
 
+            if classification not in classifaction_map: #check to see if we have seen this diagnosis
+                # if we haven't seen this classifcation, add it to the map.
+                class_number = len(classifaction_map)
+
+                classifaction_map[classification] = class_number
+            
+            patient = self.y_data_frame.index[i]
+            number = classifaction_map[classification]
+
+            line = patient + "," + str(number)
+            lines.append(line)
+
+        return lines
 
 
 
@@ -36,3 +56,5 @@ print(y_data_frame.index[0])
 path = os.path.join("rawData", "bulk_mrna_luad.h5ad")
 
 adp = AnnDataProcessor(path)
+
+adp.patientLines()
