fada6f1910bfaa882de2f7ee184ff2e2c98f414e
luciancahil <royhe62@yahoo.ca>
2024-02-21 18:39:41 -0800

    Same

--- run/Graphs/All_graph.py
+++ run/Graphs/All_graph.py
@@ -1,17 +1,35 @@
 import os
 
-def list_files_in_directory():
-    # Get the current directory path
-    current_directory = os.path.dirname(os.path.realpath(__file__))
+def write_file_contents_to_csv(directory, output_file):
+    line_set = set()
+    with open(output_file, 'w') as csv_file:
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            # Check if it's a file and not a directory
+            if os.path.isfile(file_path):
+                if(filename == "All_graph.py" or filename == "__init__.py"):
+                    continue
 
-    # List all files in the current directory
-    for filename in os.listdir(current_directory):
-        if os.path.isfile(os.path.join(current_directory, filename)):
-            if(filename == "__init__.py" or filename== "All_graph.py"):
-                continue
-            under_index = filename.index("_")
-            name = (filename[0:under_index])
-            new_name = name + "_diverse_graph.csv"
-            os.rename("run/Graphs/" + filename,"run/Graphs/" + new_name)
+                
+                under_index = filename.index("_")
+                cyto_name = filename[0:4]
+                
+                with open(file_path, 'r') as file:
+                    for line in file:
+                        if(len(line) == 0):
+                            continue
+                        parts = line.split(",")
+                        print(filename)
+                        line1 = parts[0] + "," + cyto_name + "\n"
+                        line2 = cyto_name + "," + parts[1]
+                        if(not (line1 in line_set)):
+                            csv_file.write(line1)
+                            line_set.add(line1)
+                        
+                        if(not (line2 in line_set)):
+                            csv_file.write(line2)
+                            line_set.add(line2)                          
 
-list_files_in_directory()
+current_directory = os.path.dirname(os.path.abspath(__file__))
+output_file = os.path.join(current_directory, "full_diverse.csv")
+write_file_contents_to_csv(current_directory, output_file)
