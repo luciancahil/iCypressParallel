9c49ae67a680341a510e78e527dc8f457089f365
luciancahil <royhe62@yahoo.ca>
2024-08-11 13:29:29 -0700

    Fix issue with batch_size+1 examples

--- run/main.py
+++ run/main.py
@@ -44,6 +44,23 @@ if __name__ == '__main__':
         auto_select_device()
         # Set machine learning pipeline
         datasets = create_dataset()
+        #
+        if(len(datasets[0]) % cfg.train.batch_size == 1):
+            graph_list = list(datasets[0])
+            if graph_list:
+                datasets[0] = GraphDataset(
+                                graph_list[:-1],
+                                task=cfg.dataset.task,
+                                edge_train_mode=cfg.dataset.edge_train_mode,
+                                edge_message_ratio=cfg.dataset.edge_message_ratio,
+                                edge_negative_sampling_ratio=cfg.dataset.edge_negative_sampling_ratio,
+                                resample_disjoint=cfg.dataset.resample_disjoint,
+                                minimum_node_per_graph=0)
+
+            else:
+                print("The dataset would be empty; no new dataset created.")
+
+
         loaders = create_loader(datasets)
         loggers = create_logger()
         model = create_model()
